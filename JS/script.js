41721486bff4ee467492828e87f63f7adba0d594
Dev Kumar <devkr047@gmail.com>
2024-10-16 18:08:16 +0530

    Commit 1

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,13 @@
-const container=document.getElementById('container');
-const navbar= document.getElementById('navbar');
-
-
-container.addEventListener('scroll',function(){
-    if(this.scrollTop > 0){
-navbar.style.backgroundColor='#fff';
-navbar.style.boxShadow='2px 2px 7px black';
+const container = document.getElementById('container');
+const navbar = document.getElementById('navbar');
+container.addEventListener('scroll', function () {
+    if (this.scrollTop > 0) {
+        navbar.style.backgroundColor = '#fff';
+        navbar.style.boxShadow = '2px 2px 7px black';
     }
-    else{
-        
-navbar.style.backgroundColor='transparent';
-navbar.style.boxShadow='none';
+    else {
+
+        navbar.style.backgroundColor = 'transparent';
+        navbar.style.boxShadow = 'none';
     }
 })
